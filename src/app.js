1f3ee24560c39d3c602eea51ac59f0b1628366ef
Radit <radityasurya@hotmail.com>
2019-08-13 00:56:54 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Counter from './counter.js'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Katalog</h1>
+    </div>
+  )
+}
+
+export default App
